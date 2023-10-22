56a2d32b241788416f3c0329702c9ca2395602ab
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-10-22 13:35:11 -0300

    update component cards
    Refatoração e otimização de códigos para os componentes cards, além da implementação de serviços para automatização dos componentes!

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CardsService } from 'src/app/services/cards.service';
 
 @Component({
   selector: 'app-cards',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./cards.component.css'],
 })
 export class CardsComponent {
+  public cards!: any[];
 
+  constructor(private cardsELement: CardsService) {
+    this.cards = this.cardsELement.getCards();
+  }
 }
--- src/app/services/cards.service.ts
+++ src/app/services/cards.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CardsService {
+  private cards!: any[];
 
-  constructor() { }
+  constructor() {
+    this.cards = [
+      {
+        id: 1,
+        src: '../../assets/imgs/html-5.png',
+        alt: 'html',
+        nome: 'HTML',
+      },
+      {
+        id: 2,
+        src: '../../assets/imgs/css.png',
+        alt: 'css',
+        nome: 'CSS',
+      },
+      {
+        id: 3,
+        src: '../../assets/imgs/js.png',
+        alt: 'js',
+        nome: 'JS',
+      },
+      {
+        id: 4,
+        src: '../../assets/imgs/typescript.png',
+        alt: 'ts',
+        nome: 'TS',
+      },
+      {
+        id: 5,
+        src: '../../assets/imgs/bootstrap.png',
+        alt: 'bootstrap',
+        nome: 'Bootstrap',
+      },
+      {
+        id: 6,
+        src: '../../assets/imgs/angular.png',
+        alt: 'angular',
+        nome: 'Angular',
+      },
+    ];
+  }
+
+  getCards() {
+    return this.cards;
+  }
 }

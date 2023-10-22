00bd15ec749b92c4009fe8c34c632b041ccef9a5
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-10-22 02:21:21 -0300

    Construção da Pág Not Found
    Configuração estrutural da página 404 em produção, formatação inicial pronta, dando andamento ao desenvolvimento da lógica funcional da página!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ProjetosComponent } from './pages/projetos/projetos.component';
-import { ProjetosDetalhesComponent } from './pages/projetos-detalhes/projetos-detalhes.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'projetos', component: ProjetosComponent },
-  { path: 'projetos/:id', component: ProjetosDetalhesComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent },
 ];
--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  styleUrls: ['./not-found.component.css'],
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements OnInit {
+  public contador: number = 0;
+  public show: boolean = false;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.show = !this.show;
+
+      const interval = setInterval(() => {
+        this.contador++;
+
+        if (this.contador == 5) {
+          clearInterval(interval);
+        }
+      }, 1000);
+    }, 2000);
+  }
 }

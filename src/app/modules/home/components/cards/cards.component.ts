fba58dfdbe76605aef4b12f1f2de633eaec7c5ec
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-11-06 17:19:10 -0300

    Update Animation
    Animação dos cards de habilidades concluídos!

--- src/app/modules/home/components/cards/cards.component.ts
+++ src/app/modules/home/components/cards/cards.component.ts
@@ -9,23 +9,12 @@ import { CardsService } from 'src/app/modules/home/services/cards.service';
 export class CardsComponent implements OnInit {
   @Output() public cardId = new EventEmitter();
   public cards!: any[];
-  protected animation!: string;
 
   constructor(private cardsELement: CardsService) {
     this.cards = this.cardsELement.getCards();
   }
 
-  ngOnInit(): void {
-    for (let i = 0; i < this.cards.length; i++) {
-      let divId: number = this.cards[i].id;
-
-      if (divId % 2) {
-        console.log(`São ímpares ${divId}`);
-      } else {
-        console.log(`São pares ${divId}`);
-      }
-    }
-  }
+  ngOnInit(): void {}
 
   getId(event: HTMLDivElement) {
     let id = parseInt(event.id);

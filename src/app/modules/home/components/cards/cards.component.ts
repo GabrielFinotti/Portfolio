a6574e79070db9bb69d68420c6a8507e262dcc89
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-11-06 14:41:33 -0300

    Update Animation
    Configurando as animações dos cards de habilidades

--- src/app/modules/home/components/cards/cards.component.ts
+++ src/app/modules/home/components/cards/cards.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { CardsService } from 'src/app/modules/home/services/cards.service';
 
 @Component({
@@ -6,12 +6,25 @@ import { CardsService } from 'src/app/modules/home/services/cards.service';
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.css'],
 })
-export class CardsComponent {
+export class CardsComponent implements OnInit {
   @Output() public cardId = new EventEmitter();
   public cards!: any[];
+  protected animation!: string;
 
   constructor(private cardsELement: CardsService) {
     this.cards = this.cardsELement.getCards();
+  }
+
+  ngOnInit(): void {
+    for (let i = 0; i < this.cards.length; i++) {
+      let divId: number = this.cards[i].id;
+
+      if (divId % 2) {
+        console.log(`São ímpares ${divId}`);
+      } else {
+        console.log(`São pares ${divId}`);
+      }
+    }
   }
 
   getId(event: HTMLDivElement) {

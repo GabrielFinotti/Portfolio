dbd0c9e03968b2ead9ce91ed733ff31e315a92be
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-11-08 23:58:08 -0300

    Refatoração
    Refatoração do código do componente card-perfil, e adição  de valores via serviços!

--- src/app/modules/home/pages/home.component.ts
+++ src/app/modules/home/pages/home.component.ts
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   public id!: number;
+  protected test!: Array<number>;
+
+  constructor() {
+    this.test = [1, 4, 5, 6];
+    this.test.forEach((i) => {
+      console.log(i)
+    })
+  }
   // Está pegando o valor enviado pelo elemento filho cards e anexando a variável this.id para posteriormente enviala para seu componete filho cards-descricao!
   public getIdCards(id: number) {
     this.id = id;

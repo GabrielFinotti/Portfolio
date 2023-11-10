1f72acc82e653014679a0bce2caa82bd320e64bf
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-11-09 21:03:30 -0300

    Refatoração
    Otimização e refatoração do codigo dos componentes de cards de habilidades!

--- src/app/modules/home/components/cards/cards.component.ts
+++ src/app/modules/home/components/cards/cards.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Output,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 import { CardsService } from 'src/app/modules/home/services/cards.service';
 
 @Component({
@@ -6,9 +13,12 @@ import { CardsService } from 'src/app/modules/home/services/cards.service';
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.css'],
 })
-export class CardsComponent {
+export class CardsComponent implements AfterViewInit {
   // Está emitindo um novo evento que vai ser recebido pelo componente pai!
   @Output() public cardId = new EventEmitter();
+  // Acessando as divs pela variável local de referência!
+  @ViewChildren('cardId') protected card!: QueryList<any>;
+
   public cards!: any[];
   protected id!: number;
 
@@ -16,27 +26,19 @@ export class CardsComponent {
     // Está recebendo os valores do Array dos cards via serviço!
     this.cards = this.cardsService.getCards();
   }
+  // Recupera as div após ser rendeizado, acessando seus valores pelo @ViewChildren e assim setando o intervalo de tempo para chamada da classe de animação!
+  ngAfterViewInit(): void {
+    let cont: number = 100;
+    this.card.forEach((i) => {
+      setTimeout(() => {
+        i.nativeElement.classList.add('balanco');
+      }, cont);
+      cont += 350;
+    });
+  }
   // Está recebendo o id do elemento especificado e emitindo seu valor pelo evento cardId criado para enviar seu id pro elemento pai!
   getId(event: HTMLDivElement) {
     this.id = parseInt(event.id);
     this.cardId.emit(this.id);
-  }
-  // Ao passar o mouse no elemento, ele verifica em que condição o id se encaixa e posteriormente remove a classe definida para cada condição!
-  removeAnimation(event: HTMLDivElement) {
-    this.id = parseInt(event.id);
-    if (this.id % 2 == 0) {
-      event.classList.remove('balanco-reverse');
-    } else {
-      event.classList.remove('balanco');
-    }
-  }
-  // Ao retirar o mouse do elemento, ele verifica em que condição o id se encaixa e posteriormente adiciona a classe definida para cada condição!
-  addAnimation(event: HTMLDivElement) {
-    this.id = parseInt(event.id);
-    if (this.id % 2 == 0) {
-      event.classList.add('balanco-reverse');
-    } else {
-      event.classList.add('balanco');
-    }
   }
 }

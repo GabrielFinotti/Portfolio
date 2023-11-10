7b78048c6751c4aab708bd8a46be0d7673663183
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-11-10 16:01:09 -0300

    Relógio
    Adicionado um widget de relógio animado que o usuário pode interagir para ver a data e hora, podendo expandi-lo para obter a informação ou fecha-lo!

--- src/app/shared/components/data-time/data-time.component.ts
+++ src/app/shared/components/data-time/data-time.component.ts
@@ -7,14 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataTimeComponent implements OnInit {
   public data!: Date;
+  protected rotate!: boolean;
+  protected reverse!: boolean;
 
   constructor() {
     this.data = new Date();
+    this.rotate = false;
+    this.reverse = false;
   }
 
   ngOnInit(): void {
     setInterval(() => {
       this.data = new Date();
     }, 1000);
+  }
+  // Função para executar a animação do card
+  rotateAnimation(event: HTMLImageElement) {
+    // Condição passada para a verificação e troca da animação de saida e entrada do componente
+    if (this.rotate == false) {
+      this.reverse = false;
+      this.rotate = true;
+    } else {
+      this.rotate = false;
+      this.reverse = true
+    }
   }
 }
